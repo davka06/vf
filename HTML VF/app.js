2dbb1e27e28ea7a36b842cb9614f831b2b166d27
davka06 <97178954+davka06@users.noreply.github.com>
2022-02-04 12:29:10 +0100

    Add files via upload

---
+++ HTML VF/app.js
@@ -0,0 +1,9 @@
+//console.log("hfgftyhg");
+//console.info("hfgftyhg");
+//console.error("hfgftyhg");
+//console.warn("hfgftyhg");
+//const number = 5;
+//console.log("constante: " + number + ".");
+var num = 5;
+num = 7; // num = num + 7, num = true, num = "david", num=5.55
+console.log("variable: " + num + ".");
